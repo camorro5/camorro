f84c3699c7aae695f0a8222548893531fcbac97b
camorro5 <gatygqtwdhaorwusfq@gmail.com>
2026-07-27 00:40:40 +0200

    Create BootReceiver.java

---
+++ :app/src/main/java/com/sms/audit/BootReceiver.java
@@ -0,0 +1,21 @@
+package com.sms.audit;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent serviceIntent = new Intent(context, SmsService.class);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent);
+        } else {
+            context.startService(serviceIntent);
+        }
+    }
+}
